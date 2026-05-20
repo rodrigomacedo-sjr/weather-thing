37f29961c8dfa7ca1c791beb3697d0a09318303d
rodrigomacedo-sjr <rodrigomacedo.sjr@gmail.com>
2026-05-19 22:58:47 -0300

    fix: update config files
    Signed-off-by: rodrigomacedo-sjr <rodrigomacedo.sjr@gmail.com>

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,7 +1,7 @@
 import { merge } from "webpack-merge";
 import common from "./webpack.common.js";
-import { CssMinimizerPlugin } from "css-minimizer-webpack-plugin";
-import { MiniCssExtractPlugin } from "mini-css-extract-plugin";
+import CssMinimizerPlugin from "css-minimizer-webpack-plugin";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
 
 export default merge(common, {
   mode: "production",
